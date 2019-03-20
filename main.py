976a0682558d7bfb403ef102b39f055eae83cb20
theoigen <oigenZ@icloud.com>
2019-03-20 02:54:51 +0200

    added code sample that get data from json file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, lab1!")
+import json
+
+
+def get_file_data(file_name):
+    json_file = open(file_name, "r")
+    content = json_file.read()
+    return json.loads(content)
+
+
+result = get_file_data("data.json")
+
+for i in result["contacts"]:
+    print(i["name"])
+
